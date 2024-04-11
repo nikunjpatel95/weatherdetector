59d571bd1e4fb2bc7a85b1897035cf895ee01d74
Nikunj Patel <nikunjmbpatel@gmail.com>
2024-04-11 07:02:07 -0400

    progress
    conitnuation of work

--- weather/views.py
+++ weather/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method=="POST":
+        city=request.POST["place"]      ##place is coming from the form that is in index.html file
+    else:           ## written else statement to avoid the local varible referenced  before assignment
+    ## this error always happnens anytime we are using an if statement and there is a variable being assigned without addina an else statement
+        city=""       
+    return render(request,'index.html',{'city':city})
